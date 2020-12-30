4643c332ad462175036743bbf5008cdd1c77684f
Leo-prog-design <57962025+Leo-prog-design@users.noreply.github.com>
2020-12-30 12:03:11 +0300

    тууц

--- Lb8Git/Program.cs
+++ Lb8Git/Program.cs
@@ -10,7 +10,7 @@ namespace Lb8Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Первое знакомство с GitHub");
+            Console.WriteLine("Первапвое знакомство с GitHub");
             Console.WriteLine("Изменения на сервере");
             Console.WriteLine("Ещё изменения на сервере");
         }
